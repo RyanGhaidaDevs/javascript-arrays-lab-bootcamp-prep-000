9abf85c057c08c3ea07e92f4f719ccd1539b2eba
Ryan Ghaida <ryaboug@gmail.com>
2018-02-03 22:41:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 
 var kittens = ['Milo', 'Otis', 'Garfield']
+// global variable 
 
 function destructivelyAppendKitten(name) {
 
 kittens.push(name)
 
   return kittens
+}
+
+
+function destructivelyPrependKitten(name) { 
+
+kittens.unshift(name)
+
+return kittens
 }
